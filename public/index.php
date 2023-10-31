178c7a537b33772fd3a0a37ff5e11143f31f26d3
Sheyla <SHEYLAVERGARAXD@GMAIL.COM>
2023-10-30 20:39:26 -0500

    update small

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(E_ALL);
+require_once '../vendor/autoload.php';
 
 // cargamos las variables de entorno
 try {
@@ -14,7 +15,5 @@ try {
 // Iniciamos la instancia de la base de datos
 // Y procedemos a montar la base de datos.
 $instance = \Database\MysqlConnection::getInstance();
-$instance->mountDatabase();
-
-require_once '../routes/routes.php';
+\Helpers\ServerLogger::log($instance->mountDatabase());
 
